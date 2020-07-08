95e7544c059e3fa238c92742f1a6a3a454771103
Pieter Daalder <github@tovernaar.nl>
2020-07-08 21:25:07 +0200

    Update on create_admin to enforce password reset for the new acccount.

--- create-wpadmin.php
+++ create-wpadmin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: SD Create WP Admin
  * Description: A small plugin you can use to create a new WP user account that has administrator priviliges.
- * Version: 1.0
+ * Version: 1.1
  * Author: Pieter Daalder
  * License: GPLv3
  */
@@ -13,12 +13,13 @@ if ( !class_exists( 'SD_Create_WP_Admin' ) ) {
         
         public static function create_admin() {
             $newuser = 'new-admin';
-            $pass    = 'creativepassword';
+            $pass    = wp_generate_password( 12, true, true );
             $mail    = 'you@example.com';
             if ( ! username_exists( $newuser ) ) {
                 $user_id = wp_create_user( $newuser, $pass, $mail );
                 $user = new WP_User( $user_id );
-                $user->set_role( 'administrator' );            
+                $user->set_role( 'administrator' );
+                wp_new_user_notification( $user_id, null, $notify = 'user' );
             }
         }
     }
